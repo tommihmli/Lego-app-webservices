09a8f7d3fa62367984c07dbb1195eb0ce64e5c8d
Tommi Hämäläinen <tommihmli@gmail.com>
2026-05-06 20:23:36 +0300

    Add SendData thread to LegoApp

--- twoproperties-main/src/app/LegoApp.java
+++ twoproperties-main/src/app/LegoApp.java
@@ -7,12 +7,14 @@ public class LegoApp {
 	public static void main(String[] args) {
 		RunLego runLego=new RunLego();
 		ReadData readData=new ReadData();
-
+		SendData sendData = new SendData();
 		System.out.println("Run in Threads");
 
 		Thread runLegoThread=new Thread(runLego);
 		Thread readDataThread=new Thread(readData);
+		Thread sendDataThread = new Thread(sendData);
 		runLegoThread.start(); //Saikeen kaynnistys
 		readDataThread.start(); //Virtuaalikone aloittaa saikeen kun ehtii
+		sendDataThread.start();
 	}
 }
